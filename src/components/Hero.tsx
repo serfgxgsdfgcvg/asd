c7f705dcc979de536fc2abe9650249992f3cb56f
k. <105748534+khazvek@users.noreply.github.com>
2025-07-03 10:47:56 +0200

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -162,7 +162,7 @@ export default function Hero() {
                 >
                   <div className="w-full h-full rounded-full overflow-hidden border-4 border-gray-100 dark:border-gray-800">
                     <LazyImage 
-                      src="/DSC00831.png" 
+                      src="/DSC0831.png" 
                       alt="Theo Blondel"
                       className="w-full h-full object-cover"
                     />
