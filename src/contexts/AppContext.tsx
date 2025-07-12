a39b6e6d71e257443cbda1e4fa60d63ed550f79b
serfgxgsdfgcvg <serfgxgsdfgcvg@gmail.com>
2025-07-13 01:24:59 +0200

    Add missing useApp hook export

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -13,6 +13,14 @@ interface AppContextType {
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
+export const useApp = () => {
+  const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error('useApp must be used within an AppProvider');
+  }
+  return context;
+};
+
 const translations = {
   en: {
     // Navigation
